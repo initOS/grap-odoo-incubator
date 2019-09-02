b910a4af241ddf443ab2c8ebcd04d2dc8d25fde0
Andreas Zoellner <andreas.zoellner@initos.com>
2019-09-02 14:31:49 +0200

    [MIG] Fix import statements from 'openerp' to 'odoo'

--- stock_inventory_merge/models/stock_inventory.py
+++ stock_inventory_merge/models/stock_inventory.py
@@ -4,8 +4,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from openerp import _, api, fields, models
-from openerp.exceptions import Warning as UserError
+from odoo import _, api, fields, models
+from odoo.exceptions import Warning as UserError
 
 
 class StockInventory(models.Model):
--- stock_inventory_merge/tests/test_module.py
+++ stock_inventory_merge/tests/test_module.py
@@ -3,8 +3,8 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests.common import TransactionCase
-from openerp.exceptions import Warning as UserError
+from odoo.tests.common import TransactionCase
+from odoo.exceptions import Warning as UserError
 
 
 class TestModule(TransactionCase):
